da225f8b4874d3862d78973707fefdf0eb1c4c39
Retro-Markz <cubedomarco@gmail.com>
2024-09-18 11:56:00 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
   function handlePrev() {
     if (step > 1) setStep(step - 1);
   }
-
+  e;
   function handleNext() {
     if (step < 3) setStep(step + 1);
   }
 
   return (
     <>
-      <div className="container  h-[max-content]  w-[40vw] bg-gray-200 mx-auto mt-48 rounded-xl shadow-xl ">
+      <div className="container  h-[max-content]  w-[40vw] bg-gray-200 mx-auto mt-48 rounded-3xl shadow-xl ">
         <div className="steps flex justify-between p-8 *:font-semibold ">
           <div className={step >= 1 ? "active" : "num"}>1</div>
           <div className={step >= 2 ? "active" : "num"}>2</div>
