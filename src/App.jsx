449b05e109e8b5212f2b757406c04ca5739f7dde
Retro-Markz <cubedomarco@gmail.com>
2024-09-18 07:08:49 -0700

    button and logic functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import "./App.css";
 
-const messages = ["Learn react ", "Apply for jobs", "continue improving"];
+import { useState } from "react";
+
+const messages = ["Learn React ", "Apply for Jobs", "Continue Improving"];
 
 function App() {
-  const step = 0;
+  const [step, setStep] = useState(1);
+
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
     <>
       <div className="container  h-[max-content]  w-[40vw] bg-gray-200 mx-auto mt-48 rounded-xl shadow-xl ">
         <div className="steps flex justify-between p-8 *:font-semibold ">
-          <div className={`${step >= 1 ? "active" : "num"}`}>1</div>
-          <div className={`${step >= 2 ? "active" : "num"}`}>2</div>
-          <div className={`${step >= 3 ? "active" : "num"}`}>3</div>
+          <div className={step >= 1 ? "active" : "num"}>1</div>
+          <div className={step >= 2 ? "active" : "num"}>2</div>
+          <div className={step >= 3 ? "active" : "num"}>3</div>
         </div>
 
         <p className="message text-center text-4xl font-bold pt-8 pb-8">
@@ -18,8 +29,13 @@ function App() {
         </p>
 
         <div className="buttons flex justify-around font-bold mt-4 pb-8">
-          <button className="btn">Previus</button>
-          <button className="btn">Next</button>
+          <button className="btn" onClick={() => handlePrev()}>
+            Previous
+          </button>
+
+          <button className="btn" onClick={() => handleNext()}>
+            Next
+          </button>
         </div>
       </div>
     </>
