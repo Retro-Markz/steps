e47e7d5a7cd8c19bedd938a6aefbf697f45f6f89
Retro-Markz <cubedomarco@gmail.com>
2024-09-18 04:54:05 -0700

    conditional classes logic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 
-const messages = ["Learn react", "Apply for jobs", "continue improving"];
+const messages = ["Learn react ", "Apply for jobs", "continue improving"];
 
 function App() {
+  const step = 1;
   return (
     <>
       <div className="container  h-[max-content]  w-[40vw] bg-gray-200 mx-auto mt-48 rounded-xl shadow-xl ">
         <div className="steps flex justify-between p-8 *:font-semibold ">
-          <div className="one active ">1</div>
-          <div className="two num">2</div>
-          <div className="tree num">3</div>
+          <div className={`${step >= 1 ? "active" : "num"}`}>1</div>
+          <div className={`${step >= 2 ? "active" : "num"}`}>2</div>
+          <div className={`${step >= 3 ? "active" : "num"}`}>3</div>
         </div>
 
-        <p className="message text-center font-bold pt-8 pb-8">Hello</p>
+        <p className="message text-center text-4xl font-bold pt-8 pb-8">
+          Step {step}: {messages[step - 1]}
+        </p>
 
         <div className="buttons flex justify-around font-bold mt-4 pb-8">
           <button className="btn">Previus</button>
